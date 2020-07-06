157d28262a35116ae202bbeb6ae5a8b2bc3bc793
meraihan <Rr72442667#>
2020-07-06 13:22:02 +0600

    initial structure

---
+++ src/main/java/com/wulf/systems/mes/MESSystem/MesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.wulf.systems.mes.MESSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wulf/systems/mes/MESSystem/MesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wulf.systems.mes.MESSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
